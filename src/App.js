80d7052cc2449f2f329b44e0128a5676a1c4e20f
nila <terrakkk@outlook.com>
2019-01-20 17:09:46 +0630

    fix:Change the div tag to image and style.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import IronImage from "./IronImage";
-import "./App.css";
 import smallImage from "./preloading.jpg";
+import "./styles.css";
 const hdImage = "https://images.unsplash.com/photo-1478562853135-c3c9e3ef7905";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <IronImage srcPreload={smallImage} srcLoaded={hdImage} />
+      <div className="app">
+        <div className="app-iron-image-container">
+          <div className="solitude__image-container">
+            <IronImage srcPreload={smallImage} src={hdImage} />
+          </div>
+        </div>
       </div>
     );
   }
